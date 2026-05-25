1bff708d3357ebe2c06ad554ae70d6388ed53ccd
thebigkorea <thebig@koreafnb.com>
2026-05-25 10:43:51 +0900

    service worker cache disable

--- sw.js
+++ sw.js
@@ -25,12 +25,19 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
