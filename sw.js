6284a1c8a201a0f676a07bf1e4cac692a03bbaa3
thebigkorea <thebig@koreafnb.com>
2026-05-29 12:48:38 +0900

    개발 중 서비스워커 캐시 비활성화

--- sw.js
+++ sw.js
@@ -1,29 +1,4 @@
-const CACHE_NAME = "thebigkorea-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./vehicle.html",
-  "./vehicle.css",
-  "./vehicle.js",
-  "./vehicle-admin.html",
-  "./vehicle-admin.css",
-  "./vehicle-admin.js",
-  "./trip.html",
-  "./trip.css",
-  "./trip.js",
-  "./trip-admin.html",
-  "./trip-admin.css",
-  "./trip-admin.js"
-];
-
-self.addEventListener("install", event => {
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
+const CACHE_NAME = "thebigkorea-dev-no-cache-v1";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -31,13 +6,18 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => caches.delete(key))
+      );
+    })
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" })
+  );
 });
