a890498a07b1d6389eacfd27cccd516f5d7fd665
parkss9799 <parkss9799@gmail.com>
2026-06-08 16:59:57 +0900

    본사 포털 서비스워커 캐시 초기화

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "thebigkorea-dev-no-cache-v1";
+const CACHE_NAME = "thebigkorea-hq-no-cache-v4";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -6,18 +6,16 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request, { cache: "no-store" })
+    fetch(event.request, {
+      cache: "no-store"
+    }).catch(() => fetch(event.request))
   );
 });
