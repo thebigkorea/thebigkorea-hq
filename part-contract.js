df08390bb848a13af0d64e936de8b9ca46792a91
thebigkorea <thebigj@koreafnb.com>
2026-09-02 11:46:45 +0900

    전자계약 API 주소 통일

--- part-contract.js
+++ part-contract.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbwRGQcXgYhfkTUiklPrHs4uFe7oHpgn8D_jM2jJPpU74tXr3D_h6vGMq72CHXU0EnAb/exec";
+  "https://script.google.com/macros/s/AKfycbyshRs9k9fBXHjNlHSGvpDM2ueLRVRNL3Ya_3xorvLuZ9HHc4fB8JBa6jEowDRW0ZeO/exec";
 
 let canvas;
 let ctx;
